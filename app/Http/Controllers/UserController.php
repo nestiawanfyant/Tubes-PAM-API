658454791378bbeb6abecaa5cf4930911ce452f6
Nestiawan Ferdiyanto <nestiawan.118140190@student.itera.ac.id>
2021-05-19 13:22:39 +0700

    insert kendaraan dan barang

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,7 +58,7 @@ class UserController extends Controller
         if($user){
             return response()->json([
                 'status'    => false,
-                'message'   => "Email Sudah tersedia".
+                'message'   => "Email Sudah tersedia"
             ]);
         }
 
